fad4ed4425323821ad59151c03d4718fef8c90b5
uncoyote <jeker.git@outlook.de>
2022-05-17 16:41:59 +0200

    deriving gm03 so template

--- gm03_proto/import.sh
+++ gm03_proto/import.sh
@@ -2,8 +2,9 @@
 
 java -jar ~/tools/ili2pg-4.7.0/ili2pg-4.7.0.jar \
 --dbdatabase postgres --dbusr postgres --dbpwd postgres --dbschema gm03 \
---schemaimport --models GM03_2_1Core \
-$(pwd)/GM03_2_1.ili
+--import \
+--disableValidation \
+$(pwd)/gnbe.xml
 
 
 
